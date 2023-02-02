25548190b79a257e0172a2136f067dc7694e5e76
Chaboche Pierre-Alexis <pierrealexischaboche@gmail.com>
2023-02-02 23:56:13 +0100

    about and admin

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -5,28 +5,56 @@ import "./Admin.css";
 
 const Admin = () => {
   const [content, setContent] = useState("");
+  const [aboutIds, setAboutIds] = useState([]);
+  const [editId, setEditId] = useState(null);
 
   const handleUpdate = () => {
-    if (content.paragraph && content.title) {
+    if (content.paragraph && content.title && editId !== null) {
       axios
-        .put(`${import.meta.env.VITE_BACKEND_URL}/about/1`, content)
+        .put(`${import.meta.env.VITE_BACKEND_URL}/about/${editId}`, content)
         .then(() => {
           alert("ok");
         });
     }
   };
 
+  const handleChange = async (event) => {
+    event.preventDefault();
+    setEditId(event.target.value);
+
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_BACKEND_URL}/about/${event.target.value}`
+      );
+      setContent(response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   useEffect(() => {
-    axios
-      .get(`${import.meta.env.VITE_BACKEND_URL}/about/1`)
-      .then(({ data }) => {
-        setContent(data);
-      });
+    const getAllAbout = async () => {
+      const response = await axios.get(
+        `${import.meta.env.VITE_BACKEND_URL}/about/`
+      );
+
+      setAboutIds(response.data);
+    };
+
+    getAllAbout();
   }, []);
+
   return (
     <div>
       <p>hello admin</p>
-      <input type="number" />
+
+      <label htmlFor="aboutId">Choose your about:</label>
+
+      <select value={editId} onChange={handleChange} name="editId" id="editId">
+        {aboutIds &&
+          aboutIds.map((el) => <option value={el.id}>{el.id}</option>)}
+      </select>
+
       <input
         type="text"
         value={content.title}
